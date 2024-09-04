fae24f8130d013d12dee4a24c4ef9fe5f6792dab
PhilipSteingrüber <philip.steingruber@gmail.com>
2024-09-04 11:33:22 +0200

    .

--- weatherdashboard/weather/models.py
+++ weatherdashboard/weather/models.py
@@ -5,11 +5,14 @@ from django.db import models
 # Create your models here.
 class WeatherData(models.Model):
     city = models.CharField(max_length=50, null=False, blank=False)
-    current_temperature = models.FloatField()
-    current_humidity = models.FloatField()
-    current_wind_speed = models.FloatField()
-    current_weather = models.CharField(max_length=100, null=False, blank=False)
-    timestamp = models.DateTimeField()
+    current_temperature = models.FloatField(null=True, blank=True)
+    current_feelslike = models.FloatField(null=True, blank=True)
+    current_humidity = models.FloatField(null=True, blank=True)
+    current_pressure = models.IntegerField(null=True, blank=True)
+    current_wind_speed = models.FloatField(null=True, blank=True)
+    current_uv_index = models.FloatField(null=True, blank=True)
+    current_weather_text = models.CharField(max_length=100, null=False, blank=False)
+    current_weather_icon_url = models.CharField(max_length=200, null=False, blank=False)
 
     def __str__(self):
         return f'{self.city}, {self.current_temperature}, {self.current_humidity}, {self.current_wind_speed}'
--- weatherdashboard/weather/views.py
+++ weatherdashboard/weather/views.py
@@ -5,8 +5,10 @@ import requests
 from django.shortcuts import render
 from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
+from .models import WeatherData
 
 from .constants import API_KEY
+from geopy.exc import GeocoderUnavailable, GeopyError
 
 tf = TimezoneFinder()
 geolocator = Nominatim(user_agent="weatherdashboard")
@@ -20,8 +22,21 @@ def get_local_time(city: str):
 
 def get_weather(city: str):
     query_url = f'https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no'
-    response = requests.get(query_url)
-    return response.json()
+    data = requests.get(query_url).json()['current']
+
+    weather = WeatherData.objects.create()
+    weather.city = city
+    weather.current_temperature = data['temp_c']
+    weather.current_feelslike = data['feelslike_c']
+    weather.current_humidity = data['humidity']
+    weather.current_pressure = int(data['pressure_mb'])
+    weather.current_wind_speed = round(data['wind_kph'] / 3.6, 1) # Convert to m/s
+    weather.current_uv_index = data['uv']
+    weather.current_weather_text = data['condition']['text']
+    weather.current_weather_icon_url = data['condition']['icon']
+    weather.save()
+
+    return weather
 
 
 def get_country(city: str):
@@ -40,8 +55,13 @@ def index(request):
         city = 'Gothenburg'
 
     weather_data = get_weather(city)
-    current_time = get_local_time(city)
-    country = get_country(city)
+    try:
+        current_time = get_local_time(city)
+        country = get_country(city)
+    except GeopyError:
+        current_time = dt.now().time().strftime("%H:%M")
+        country = 'Sweden'
+        print('Geocoder failed, setting defaults')
 
     context = {'weather_data': weather_data, 'city': city.title(), 'current_time': current_time, 'country': country}
     return render(request, 'weather/layout.html', context=context)
